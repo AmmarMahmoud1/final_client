b961224e9aff7b855da3e56d6c2eb22f2c788114
Koyalchoure89 <onekumud@gmail.com>
2023-05-31 19:36:18 +0200

    i am here

--- src/App.js
+++ src/App.js
@@ -1,57 +1,44 @@
-
-import React from 'react';
-import { useState,useEffect }    from 'react';
-import axios from 'axios';
-import { toastError } from './lib/toastError';
-import { Routes, Route } from 'react-router-dom';
-import Layout  from './pages/Layout';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login'
-import NotFound from './pages/NotFound';
-import Home from './pages/Home'
-import NavBar from './components/NavBar';
-import Message from './components/Message'
-import Jobs from './components/Jobs';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toastError } from "./lib/toastError";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Message from "./components/Message";
+import Jobs from "./components/Jobs";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import AddPost from './pages/AddPost';
-import AllMessages from './components/AllMessages';
-import RealEstate from './components/RealEstate';
-import Pets from './components/Pets';
-import Autos from './components/Autos';
-import Services from './components/Services';
-import Furniture  from './components/Furniture'
-import Electronics from './components/Electronics'
-import Offers from './components/Offers';
-import Footer from './components/Footer';
+import AddPost from "./pages/AddPost";
+import AllMessages from "./components/AllMessages";
+import RealEstate from "./components/RealEstate";
+import Pets from "./components/Pets";
+import Autos from "./components/Autos";
+import Services from "./components/Services";
+import Furniture from "./components/Furniture";
+import Electronics from "./components/Electronics";
+import Offers from "./components/Offers";
+import Footer from "./components/Footer";
+import ItemCard from "./ItemCard";
 
-
-
-
-
-
-
-
-const  App  = () => {
+const App = () => {
   const [isAuth, setIsAuth] = useState(false);
   const [user, setUser] = useState({});
   const [gotCookie, setGotCookie] = useState(false);
-  
-  const url = 'http://localhost:5000/api/user/me'
+
+  const url = "http://localhost:5000/api/user/me";
   useEffect(() => {
     const checkToken = async () => {
       try {
-        const { data } = await axios(url
-          ,
-          {
-            withCredentials: true,
-          }
-        );
+        const { data } = await axios(url, {
+          withCredentials: true,
+        });
         setUser(data);
         setIsAuth(true);
-        
-        
       } catch (error) {
         toastError(error.message);
       }
@@ -59,40 +46,61 @@ const  App  = () => {
     checkToken();
   }, [gotCookie]);
 
+  function App() {
+    return (
+      <div>
+        {/* Other components */}
+        <ItemCard />
+        {/* Other components */}
+      </div>
+    );
+  }
+
   return (
     <>
-       < NavBar  isAuth={isAuth}
-              setIsAuth={setIsAuth}
-              setUser={setUser}
-              setGotCookie={setGotCookie}/>
-       <Routes>
-        <Route path='/' element={
-        <Layout
+      <NavBar
+        isAuth={isAuth}
+        setIsAuth={setIsAuth}
+        setUser={setUser}
+        setGotCookie={setGotCookie}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout
               isAuth={isAuth}
               setIsAuth={setIsAuth}
               setUser={setUser}
-              setGotCookie={setGotCookie} />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login isAuth={isAuth} setGotCookie={setGotCookie} />} />
-          <Route path='/sign-up' element={<SignUp isAuth={isAuth} setGotCookie={setGotCookie} />} />
-          <Route path='/add' element={<AddPost />} />
-          <Route path='/messages' element ={< AllMessages />}    />
-          <Route path='/realestate' element ={< RealEstate />}    />
-          <Route path='/jobs' element ={< Jobs />}    />
-          <Route path='/furniture' element ={< Furniture />}    />
-          <Route path='/electronics' element ={< Electronics />}    />
-          <Route path='/pets' element ={< Pets />}    />
-          <Route path='/services' element ={< Services />}    />
-          <Route path='/autos' element ={< Autos />}    />
-          <Route path='/offers' element ={< Offers />}    />
-          <Route path='404' element={<NotFound />} />
-          </Routes>
+              setGotCookie={setGotCookie}
+            />
+          }
+        />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login isAuth={isAuth} setGotCookie={setGotCookie} />}
+        />
+        <Route
+          path="/sign-up"
+          element={<SignUp isAuth={isAuth} setGotCookie={setGotCookie} />}
+        />
+        <Route path="/add" element={<AddPost />} />
+        <Route path="/messages" element={<AllMessages />} />
+        <Route path="/realestate" element={<RealEstate />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/furniture" element={<Furniture />} />
+        <Route path="/electronics" element={<Electronics />} />
+        <Route path="/pets" element={<Pets />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/autos" element={<Autos />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="404" element={<NotFound />} />
+      </Routes>
 
-          <Footer />
-      </>
-)
+      <Footer />
+    </>
+  );
 };
-         
-
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,54 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import {toastError}  from '../lib/toastError';
-import { Button } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
-import RealEstate from '../components/RealEstate';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toastError } from "../lib/toastError";
+import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import RealEstate from "../components/RealEstate";
 
 const Home = () => {
+  const [allPost, setAllPosts] = useState();
 
-    const [allPost, setAllPosts] = useState();
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios("http://localhost:5000/api");
+        console.log(data);
+        setAllPosts(data);
+      } catch (error) {
+        toastError(error.message || "No posts,  Sorry..!");
+      }
+    })();
+  }, []);
 
-    useEffect(() => {
-        (async () => {
-          try {
-         
-            const { data } = await axios('http://localhost:5000/api');
-            console.log(data);
-            setAllPosts(data);
-            
-          } catch (error) {
-            toastError(
-              error.message || 'No posts,  Sorry..!'
-            );
-            
-          }
-        })();
-      }, []);
+  function ItemCard() {
+    return (
+      <div>
+        <a href="#"></a>
+        <div className="item-card-img">
+          <img
+            src="../assets/images/products/h4.png"
+            alt="img"
+            className="rounded-top-7"
+          />
+          <img
+            src="../assets/images/products/h4.png"
+            alt="img"
+            className="rounded-top-7"
+          />
+        </div>
+        <div className="item-card-text">
+          <h4 className="mb-0">
+            Home &amp; Furniture<span>(45)</span>
+          </h4>
+        </div>
+      </div>
+    );
+  }
 
-
-      return (
-        <>
-         
-       {/* { 
+  return (
+    <>
+      {/* { 
         (!allPost ? <div>loading..... </div> 
        
           : allPost.map(
@@ -89,12 +105,9 @@ const Home = () => {
         
         ))} */}
 
-        <RealEstate allPost={allPost} />
-        </>
-
-   
-      )
-}
-
+      <RealEstate allPost={allPost} />
+    </>
+  );
+};
 
 export default Home;
